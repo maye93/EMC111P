473b042eeb96c1024dbaaa526fdd9eb27ddb4a8b
shaniamitsui <shaniamitsui@gmail.com>
2020-10-15 23:44:44 +0800

    initial commit

--- Week11/js/app.js
+++ Week11/js/app.js
@@ -3,7 +3,7 @@ const CLOCK = new THREE.Clock();
 const FOV = 75;
 const NEAR = 0.1;
 const FAR = 1000;
-const MAXPARTICLES = 2000;
+const MAXPARTICLES = 5000;
 const RENDERER = new THREE.WebGLRenderer();
 let deltaTime;
 
@@ -21,12 +21,33 @@ let camera = new THREE.PerspectiveCamera(
   FAR
 );
 
-//position
+//camera position
 camera.position.x = 0;
 camera.position.y = 0;
 camera.position.z = 100;
 
 camera.lookAt(new THREE.Vector3());
+
+///////////////////////////
+/////     GALAXY     /////
+/////////////////////////
+
+//galaxy geometry
+let galaxyGeo = new THREE.BoxGeometry(3840, 2400, 1);
+
+//galaxy texture
+let galaxyTex = new THREE.TextureLoader().load('images/galaxy.jpg');
+
+//galaxy material
+let galaxyMat = new THREE.MeshBasicMaterial({map: galaxyTex});
+
+//combine all of the galaxy components as one
+let galaxy = new THREE.Mesh(galaxyGeo, galaxyMat);
+
+//galaxy position
+galaxy.position.z = -900;
+
+SCENE.add(galaxy);
 
 ///////////////////////////
 /////     SATURN     /////
@@ -36,18 +57,13 @@ camera.lookAt(new THREE.Vector3());
 let saturnGeo = new THREE.SphereGeometry(50, 30, 30);
 
 //saturn texture
-let saturnTex = new THREE.TextureLoader().load('images/8k_saturn.jpg');
+let saturnTex = new THREE.TextureLoader().load('images/saturn.jpg');
 
 //saturn material
 let saturnMat = new THREE.MeshBasicMaterial({map: saturnTex});
 
 //combine all of the saturn components as one
 let saturn = new THREE.Mesh(saturnGeo, saturnMat);
-
-//saturn position
-saturn.position.x = 0;
-saturn.position.y = 0;
-saturn.position.z = 0;
 
 SCENE.add(saturn);
 
@@ -70,10 +86,14 @@ for (let i = 0; i < MAXPARTICLES; i++) {
 }
 
 //ring texture
-let ringTex = new THREE.TextureLoader().load('images/particle.png');
+let ringTex = new THREE.TextureLoader().load('images/dot.png');
 
 //ring material
-let ringMat = new THREE.PointsMaterial({map: ringTex});
+let ringMat = new THREE.PointsMaterial({
+  map: ringTex,
+  alphaTest: 0.5,
+  transparent: true
+});
 
 //combine all of the ring components as one
 let ring = new THREE.Points(ringGeo, ringMat);
